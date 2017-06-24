b2a45c734fe74d9ed997fab2c4e5e8db1fb88e57
Cary Cooper <Cary Cooper>
2017-06-24 12:18:36 -0400

    Webpack running

---
+++ app/config/routes.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Route, IndexRoute, Router, browserHistory } from "react-router";
+import Main from "../components/Main";
+
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+    </Route>
+  </Router>
+);
+export default routes;
